ec6df0186765921189345f13f77bc289569bfd06
giorgiabes <abesadze.george@gmail.com>
2024-05-14 23:21:21 +0400

    Data for countries, step 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 const App = () => {
   const [value, setValue] = useState("");
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(null);
+  const [countryDetailes, setCountryDetailes] = useState(null);
 
   useEffect(() => {
     axios
@@ -14,27 +16,38 @@ const App = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (country) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+        .then((response) => {
+          setCountryDetailes(response.data);
+        });
+    }
+  }, [country]);
+
   const handleChange = (e) => {
     setValue(e.target.value);
-  };
-
-  const onSearch = (e) => {
-    e.preventDefault();
-    setValue(value);
   };
 
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(value.toLowerCase())
   );
 
-  console.log("no of filtered countries is:", filteredCountries.length);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (filteredCountries.length === 1) {
+      setCountry(filteredCountries[0].name.common);
+    }
+    setValue("");
+  };
 
   const countriesToShow = () => {
     if (filteredCountries.length === countries.length) {
       return;
     } else if (filteredCountries.length >= 10) {
       return <p>Too many matches, specify another filter.</p>;
-    } else if (filteredCountries.length > 1) {
+    } else if (filteredCountries.length >= 1) {
       return (
         <ul>
           {filteredCountries.map((country) => (
@@ -42,19 +55,40 @@ const App = () => {
           ))}
         </ul>
       );
-    } else if (filteredCountries.length === 1) {
-      return <p>Country is {filteredCountries[0].name.common}</p>;
     } else {
       return <p>There is no match</p>;
     }
   };
 
+  const countryDataToShow = () => {
+    if (!countryDetailes) {
+      return null;
+    }
+
+    const { name, capital, area, languages } = countryDetailes;
+
+    return (
+      <div>
+        <h1>{name.common}</h1>
+        <p>
+          capital: {capital} <br /> area: {area}
+        </p>
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <form onSubmit={onSearch}>
+      <form onSubmit={handleSearch}>
         find countries <input value={value} onChange={handleChange} />
       </form>
-      {countriesToShow()}
+      {country ? countryDataToShow() : countriesToShow()}
     </div>
   );
 };
